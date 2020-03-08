c1733256ac884f733fe19b083981476007f9f4d5
米斯特王 <05crazy16@sina.com>
2020-03-08 16:41:12 +0800

    打包文件

--- 6-css-loader/webpack.dev.config.js
+++ 6-css-loader/webpack.dev.config.js
@@ -1,11 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 通过 npm 安装
+
 // 开发环境
-module.exports = {
+config = {
     // 入口
     entry: {
         // 可以有多个入口, 也可以有一个, 如果有一个就默认从这一个入口开始解析
         "main": "./main.js"
     },
     output: {
+        path: path.resolve('./dist'),// 相对转绝对
         filename: "./build.js"
     },
     // 声明模块 (包含各个loader)
@@ -39,11 +43,19 @@ module.exports = {
                 // 编码会和html混在一起, 一方面可读性差, 另一方面加大了html页面的大小
                 // 反而加大了下载页面的大小, 得不偿失, 所以设置一个合理的limit是有必要的
                 test:/\.(jpg|png|gif|svg)$/,
-                loader:'url-loader?limit=60000'// 大于字节数图片会以base64显示
+                loader:'url-loader?limit=60000'// limit数值大于字节数图片会以base64显示
                 // loader:'url-loader'
             }
 
         ]
     },
+    // 插件
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'// 参照物
+        })
+    ],
     watch: true,// 文件监视改动 自动产出build.js
 }
+
+module.exports = config;
